b39674b9aad8d88821e2f324334ae9fb1cd51a68
JaehongLIM2 <nu0044@naver.com>
2025-06-25 23:57:35 +0900

    css 수정 및 bootstrap 반영

--- sql/prj2.sql
+++ sql/prj2.sql
@@ -13,9 +13,9 @@ DROP TABLE board;
 CREATE TABLE board
 (
     id      BIGINT AUTO_INCREMENT NOT NULL,
-    title   VARCHAR(255)          NOT NULL,
-    content VARCHAR(255)          NULL     DEFAULT '내용 없음',
-    writer  VARCHAR(255)          NOT NULL,
+    title   VARCHAR(100)          NOT NULL,
+    content VARCHAR(1000)          NULL     DEFAULT '내용 없음',
+    writer  VARCHAR(50)          NOT NULL,
     created datetime              NOT NULL DEFAULT NOW(),
     views   BIGINT                NOT NULL,
     CONSTRAINT pk_board PRIMARY KEY (id)
